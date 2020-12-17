f9afde430d7b530cda5f9bb35c342e6cb2ebe496
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2020-12-17 08:08:55 +0100

    Add files via upload

---
+++ Watch-Store/script.js
@@ -0,0 +1,7 @@
+// toggling menu
+function toggle () {
+    const toggle = document.querySelector('.toggle');
+    const banner = document.querySelector('.banner');
+    toggle.classList.toggle('active');
+    banner.classList.toggle('active');
+}
